000b0bccf3c2b9b5b679947e4a64d48e44556356
Arunchandra <ahalemani7@gmail.com>
2024-02-09 12:28:10 +0530

    message after counter reaches 0

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-let date = document.querySelector(".date").innerText;
-let display = document.querySelectorAll('input');
+let date = document.querySelector('.date').innerText;
+let display = document.querySelectorAll('.remainingTime');
 
 function clock(){
     let endDate = new Date(date);
-    let currentDate = new Date();
-    let seconds = (endDate - currentDate) / 1000;
-
-    if(seconds < 0) return;
-
-    let days = Math.floor(seconds / (24*60*60));
-    let hours = Math.floor(seconds / (60 * 60) % 24);
-    let minutes = Math.floor(seconds / 60 % 60);
-    seconds = Math.floor(seconds % 60);
+    let currDate = new Date();
+    let seconds = (endDate - currDate) / 1000;
     
+    if(seconds < 0){
+        let message = document.createElement('h1');
+        let innerDiv = document.querySelector('.timeData');
+        message.innerText = "Happy New Year";
+        message.style.marginTop= "50px";
+        innerDiv.after(message);
+        clearInterval(interval);
+        return;
+
+    }
+
+    let days = Math.floor(seconds /(24*60*60));
+    let hours = Math.floor(seconds / (60 * 60) % 24);
+    let minutes = Math.floor(seconds / (60) % 60);
+    seconds = Math.floor(seconds % 60);
+
     display[0].value = days;
     display[1].value = hours;
     display[2].value = minutes;
     display[3].value = seconds;
 }
 
-setInterval( () => {
+let interval = setInterval(()=>{
     clock();
 }, 1000);
